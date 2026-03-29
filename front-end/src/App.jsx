33186045f03e880daa5f92e0ded24734bda7db90
Chinwe Otti <chinweotutootti@gmail.com>
2026-03-29 03:03:22 -0400

    worked on landing, login, signup pages with styling

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 
 import AddExpense from "./pages/AddExpense.jsx"
 import Budget from "./pages/Budget.jsx"
@@ -22,60 +22,76 @@ import Logo from "./components/Logo.jsx"
 
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [expenses, setExpenses] = useState([]);
-  const [pendingExpense, setPendingExpense] = useState(null);
+const NO_NAV_PAGES = ["/", "/login", "/signup"]
 
-  function deleteExpense(id) {
-    setExpenses((prev) => prev.filter((expense => expense.id !== id)));
-  }
-  function deleteCategory(categoryName) {
-    setExpenses((prev) =>
-    prev.filter((expense) => expense.category !== categoryName));
-  }
+function AppContent({ expenses, setExpenses, pendingExpense, setPendingExpense, deleteExpense, deleteCategory }) {
+  const location = useLocation()
+  const showNav = !NO_NAV_PAGES.includes(location.pathname)
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Logo />
+      {showNav && <Navbar />}
+      {showNav && <Logo />}
 
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
 
-          <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
 
-          <Route path="/expenses" element={<ExpenseTracking expenses={expenses}/>} />
-          <Route path="/expenses/list" element={<ExpenseList 
+        <Route path="/expenses" element={<ExpenseTracking expenses={expenses} />} />
+        <Route path="/expenses/list" element={<ExpenseList
           expenses={expenses}
           deleteExpense={deleteExpense}
           deleteCategory={deleteCategory}
-          />} />
-          <Route path="/expenses/add" element={<AddExpense setPendingExpense={setPendingExpense}/>} />
-          <Route path="/expenses/confirm" element={
-            <ConfirmExpense
+        />} />
+        <Route path="/expenses/add" element={<AddExpense setPendingExpense={setPendingExpense} />} />
+        <Route path="/expenses/confirm" element={
+          <ConfirmExpense
             pendingExpense={pendingExpense}
             expenses={expenses}
             setExpenses={setExpenses}
             setPendingExpense={setPendingExpense}
-            />} />
-          <Route path="/expenses/info/:id" element={<ExpenseInfo expenses={expenses} setExpenses={setExpenses}/>} />
+          />} />
+        <Route path="/expenses/info/:id" element={<ExpenseInfo expenses={expenses} setExpenses={setExpenses} />} />
 
-          <Route path="/budget" element={<Budget />} />
-          <Route path="/budget/create" element={<CreateBudget />} />
-          <Route path="/budget/report" element={<BudgetReport />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/budget/create" element={<CreateBudget />} />
+        <Route path="/budget/report" element={<BudgetReport />} />
 
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/edit" element={<EditProfile />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/edit" element={<EditProfile />} />
 
-          <Route path="/policies" element={<Policies />} />
-        </Routes>
-      
-      </BrowserRouter>
+        <Route path="/policies" element={<Policies />} />
+      </Routes>
     </>
+  )
+}
+
+function App() {
+  const [expenses, setExpenses] = useState([])
+  const [pendingExpense, setPendingExpense] = useState(null)
+
+  function deleteExpense(id) {
+    setExpenses((prev) => prev.filter((expense) => expense.id !== id))
+  }
+
+  function deleteCategory(categoryName) {
+    setExpenses((prev) => prev.filter((expense) => expense.category !== categoryName))
+  }
+
+  return (
+    <BrowserRouter>
+      <AppContent
+        expenses={expenses}
+        setExpenses={setExpenses}
+        pendingExpense={pendingExpense}
+        setPendingExpense={setPendingExpense}
+        deleteExpense={deleteExpense}
+        deleteCategory={deleteCategory}
+      />
+    </BrowserRouter>
   )
 }
 
